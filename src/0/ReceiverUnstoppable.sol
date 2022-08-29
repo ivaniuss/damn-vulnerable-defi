e839d774c83cc08c00ca34622eb08c1314b62716
ivaniuss <ivan.emi94@gmail.com>
2022-08-28 22:06:59 -0500

    started

--- src/0/ReceiverUnstoppable.sol
+++ src/0/ReceiverUnstoppable.sol
@@ -4,7 +4,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../unstoppable/UnstoppableLender.sol";
+import "./UnstoppableLender.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
---
+++ test/0.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/0/UnstoppableLender.sol";
+import "../src/0/ReceiverUnstoppable.sol";
+
+contract UnstoppableTest {
+    function setUp() external {
+
+    }
+    
+    function testComplete() external {
+
+    }
+}
